2ce61f85842348da736f91df9449bf722e2b5db9
darkringfire <darkringfire@gmail.com>
2023-05-05 15:47:50 +0800

    Complete Task 3

---
+++ task-3/homo.py
@@ -0,0 +1,27 @@
+# Homo habilis
+# H. rudolfensis
+# H. rudolfensis
+# H. georgicus
+# H. ergaster
+# H. antecessor
+# H. cepranensis
+# H. heidelbergensis
+# H. rhodesiensis
+# H. neanderthalensis
+# H. sapiens sapiens
+
+print('Введите 11 стадиий эволюции человека:')
+s1 = input()
+s2 = input()
+s3 = input()
+s4 = input()
+s5 = input()
+s6 = input()
+s7 = input()
+s8 = input()
+s9 = input()
+s10 = input()
+s11 = input()
+
+print('Ваш ответ:', end=' ')
+print(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, sep=' => ')
---
+++ task-3/pet.py
@@ -0,0 +1,9 @@
+print('Введите даднные птомца:')
+print('Вид: ', end='')
+kind = input()
+print('Возраст: ', end='')
+age = input()
+print('Кличка: ', end='')
+name = input()
+
+print('Это {} по кличке "{}". Возраст: {} года.'.format(kind, name, age))
